f61802eec42fbc2e4f390c214c51e7ac94ceaac5
David Elster <davidandtheweb@gmail.com>
2019-10-05 15:51:40 -0400

    v1.0.0 - Renamed 'Archetypes' to 'Meanings', updated register hook

--- project-mm-plugin.php
+++ project-mm-plugin.php
@@ -1,94 +1,77 @@
 <?php
 /*
- * Plugin Name:  Project MM Plugin
+ * Plugin Name:  MyMythos - Custom Decoupling
  * Plugin URI:   https://github.com/delster/project-mm-plugin
- * Description:  This plugin contains any WP-based custom code needed for Project MM.
- * Version:      0.1.0
+ * Description:  This plugin contains any WP-based custom code needed to serve MyMythos headlessly.
+ * Version:      1.0.0
  * Author:       David Elster
  * Author URI:   https://github.com/delster/
  * License:      MIT
  * Text Domain:  projmmp
  */
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined( 'ABSPATH' ) or die();
 
-# Register CPTs (Archetypes)
-add_action( 'init', 'pmmp_register_cpts' );
-function pmmp_register_cpts() {
-	/**
-	 * Post Type: Archetypes.
-	 */
-	$labels = array(
-		"name"          => __( "Archetypes", "projmmp" ),
-		"singular_name" => __( "Archetype", "projmmp" ),
-		"edit_item"     => __( "Add New Archetype", "projmmp" ),
-		"new_item"      => __( "New Archetype", "projmmp" ),
-		"view_item"     => __( "View Archetype", "projmmp" ),
-		"view_items"    => __( "View Archetypes", "projmmp" ),
-		"search_items"  => __( "Search Archetypes", "projmmp" ),
-		"not_found"     => __( "No Archetypes found", "projmmp" ),
-		"not_found_in_trash"  => __( "No Archetypes found in Trash", "projmmp" ),
-		"all_items" => __( "All Archetypes", "projmmp" ),
-		"archives" => __( "Archetype Archives", "projmmp" ),
-		"attributes" => __( "Archetype Attributes", "projmmp" ),
-		"insert_into_item" => __( "Insert into Archetype", "projmmp" ),
-		"uploaded_to_this_item" => __( "Uploaded to this Archetype", "projmmp" ),
-	);
-	$args = array(
-		"label" => __( "Archetypes", "projmmp" ),
-		"labels" => $labels,
-		"description" => "This is the \"glue\" between posts and users. The \"meanings\" archive.",
-		'show_in_graphql' => true,
-		'graphql_single_name' => 'Archetype',
-		'graphql_plural_name' => 'Archetypes',
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"delete_with_user" => false,
-		"show_in_rest" => true,
-		"rest_base" => "",
-		"rest_controller_class" => "WP_REST_Posts_Controller",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"menu_icon" => 'dashicons-visibility',
-		"show_in_nav_menus" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "archetype", "with_front" => true ),
-		"query_var" => true,
-		"supports" => array( "title", "thumbnail", "excerpt", "custom-fields" ),
-	);
-	register_post_type( "archetype", $args );
+# Register CPT: Meanings and expose it to GraphQL.
+function register_meanings_cpt() {
+  /**
+   * Post Type: Meanings.
+   */
+  $labels = array(
+    "name"          => __( "Meanings", "projmmp" ),
+    "singular_name" => __( "Meaning", "projmmp" ),
+    "menu_name"     => __( "Meanings", "projmmp" ),
+    "all_items"     => __( "All Meanings", "projmmp" ),
+    "add_new"       => __( "Add New", "projmmp" ),
+    "add_new_item"  => __( "Add New Meaning", "projmmp" ),
+    "edit_item"     => __( "Edit Meaning", "projmmp" ),
+    "new_item"      => __( "New Meaning", "projmmp" ),
+    "view_item"     => __( "View Meaning", "projmmp" ),
+    "view_items"    => __( "View Meanings", "projmmp" ),
+    "search_items"  => __( "Search Meanings", "projmmp" ),
+    "not_found"     => __( "No Meanings Found", "projmmp" ),
+    "not_found_in_trash"    => __( "No Meanings in the Trash", "projmmp" ),
+    "parent_item_colon"     => __( "Parent Meaning:", "projmmp" ),
+    "featured_image"        => __( "Featured Image of this Meaning", "projmmp" ),
+    "set_featured_image"    => __( "Set Featured Image of this Meaning", "projmmp" ),
+    "remove_featured_image" => __( "Remove Featured Image for this Meaning", "projmmp" ),
+    "use_featured_image"    => __( "Use as Featured Image of this Meaning", "projmmp" ),
+    "archives"              => __( "Meaning Archives", "projmmp" ),
+    "insert_into_item"      => __( "Insert into Meaning", "projmmp" ),
+    "uploaded_to_this_item" => __( "Uploaded to this Meaning", "projmmp" ),
+    "filter_items_list"     => __( "Filter Meanings", "projmmp" ),
+    "items_list_navigation" => __( "Meanings List Navigation", "projmmp" ),
+    "items_list"            => __( "Meanings List", "projmmp" ),
+    "attributes"            => __( "Meanings Attributes", "projmmp" ),
+    "name_admin_bar"        => __( "Meaning", "projmmp" ),
+    "parent_item_colon"     => __( "Parent Meaning:", "projmmp" ),
+  );
+  $args = array(
+    "label"                 => __( "Meanings", "projmmp" ),
+    "labels"                => $labels,
+    'show_in_graphql'       => true,
+    'graphql_single_name'   => 'Meaning',
+    'graphql_plural_name'   => 'Meanings',
+    "description"           => "Meanings are the core taxonomy of the site.",
+    "public"                => true,
+    "publicly_queryable"    => true,
+    "show_ui"               => true,
+    "delete_with_user"      => false,
+    "show_in_rest"          => true,
+    "rest_base"             => "",
+    "rest_controller_class" => "WP_REST_Posts_Controller",
+    "has_archive"           => false,
+    "show_in_menu"          => true,
+    "show_in_nav_menus"     => true,
+    "exclude_from_search"   => false,
+    "capability_type"       => "post",
+    "map_meta_cap"          => true,
+    "hierarchical"          => false,
+    "rewrite"               => array( "slug" => "meaning", "with_front" => true ),
+    "query_var"             => true,
+    "menu_position"         => 6,
+    "menu_icon"             => "dashicons-format-status",
+    "supports"              => array( "title", "editor", "thumbnail", "excerpt", "custom-fields", "revisions", "author" ),
+  );
+  register_post_type( "meaning", $args );
 }
-
-# Expose Archetypes ACF Fields to GraphQL
-add_action( 'graphql_archetype_fields', function( $fields ) {
-	$fields['related_posts'] = [
-		'type'    => \WPGraphQL\Types::list_of(\WPGraphQL\Types::post_object('post')),
-		'resolve' => function( \WP_Post $post ) {
-			$arr = array();
-			$rposts = get_post_meta( $post->ID, 'related_posts', true );
-			foreach ($rposts as $rpost) {
-				$post = get_post( $rpost );
-				array_push($arr, $post);
-			}
-			return $arr!==null?$arr:null;
-		},
-	];
-
-	$fields['related_users'] = [
-		'type'    => \WPGraphQL\Types::list_of(\WPGraphQL\Types::user()),
-		'resolve' => function( \WP_Post $post ) {
-			$arr = array();
-			$rusers = get_post_meta( $post->ID, 'related_users', true );
-			foreach ($rusers as $ruser) {
-				$user = get_post( $ruser );
-				array_push($arr, $user);
-			}
-			return $arr!==null?$arr:null;
-		},
-	];
-
-	return $fields;
-});
+add_action( 'init', 'register_meanings_cpt' );
